b4f65e82834e040207560ec1d76c9f282693c271
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-12-11 20:46:36 +0800

    Acceleration improvement (500-1000)

--- auto_rex/main.py
+++ auto_rex/main.py
@@ -6,20 +6,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-
+object = mss.mss().monitors[0]
+object['left'] = 130
+object['top'] = 310
+object['width'] = 40
+object['height'] = 35
 start_time = time.time()
-speed = 0.035
+sleep = 0.17
+speed = 0.007
 
 while True:
-    if time.time() - start_time > 1.3:
+    if time.time() - start_time > 8:
         start_time = time.time()
-        speed /= 4
+        object['width'] += 1
+        sleep -= speed
 
-    object = mss.mss().monitors[0]
-    object['left'] = 125
-    object['top'] = 310
-    object['width'] = 25
-    object['height'] = 35
+        print('sleep: ', sleep, ', speed: ', speed, ', width: ', object['width'])
+
+    if 0.05 <= sleep <= 0.10:
+        speed = 0.008
+    if sleep <= 0.05:
+        speed = 0
 
     obj = np.array(mss.mss().grab(object))
     obj = cv2.cvtColor(obj, cv2.COLOR_BGRA2GRAY)
@@ -28,9 +35,9 @@ while True:
     #pytg.keyDown('down')
     if obj.mean() < 255:
         pytg.keyDown('up')
-        time.sleep(0.17)
+        time.sleep(sleep)
         pytg.keyDown('down')
-        pytg.sleep(speed)
+        time.sleep(0.0001)
         pytg.keyUp('down')
 
     # plt.imshow(img)
